e11e1ba3015666be261bb090c97f912e22895122
lry <lry-git@qq.com>
2023-03-07 15:42:55 +0800

    alias finder 0.0.2

--- include/Stat/CFGStat.h
+++ include/Stat/CFGStat.h
@@ -34,52 +34,11 @@ public:
     }
 
     const std::vector<ASTFunction *>& getFunctions(){
-        return callgraph->getTopLevelFunctions();
-        // for (auto fun : topLevelFuncs) {
-        //     const FunctionDecl *funDecl = manager->getFunctionDecl(fun);
-        //     callgraph->getChildren(fun);
-        //     if (funDecl->getQualifiedNameAsString() == "main") {
-        //     entryFunc = fun;
-        //     return;
-        //     }
-        // }
-        // entryFunc = nullptr;
+        return callgraph->getTopLevelFunctions();       
     }
 
-    void statCFGs() {
-        for(auto fun : callgraph->getTopLevelFunctions()){
-            auto& cfg=manager->getCFG(fun);
-            if(cfg==NULL){
-                continue;
-            }
-            dumpCFG(cfg);
-            for (auto I = cfg->begin(), E = cfg->end() ; I != E ; ++I) {
-                // Skip the entry block, because we already printed it.
-                if (&(**I) == &cfg->getEntry() || &(**I) == &cfg->getExit())
-                    continue;
-                const CFGBlock *block=*I;
-                for(auto eleIt=block->begin(); eleIt!=block->end(); eleIt++){
-                    auto& ele=*eleIt;
-                    auto kind=ele.getKind();
-                    eleIt->dump();
-                    if(kind==CFGElement::Kind::Statement){
-                        CFGStmt cfgStmt = ele.castAs<CFGStmt>();
-                        const Stmt *stmt = cfgStmt.getStmt();
-                        assert(stmt != nullptr && "Expecting non-null Stmt");
-                        if (const BinaryOperator* binOp = dyn_cast<BinaryOperator>(stmt)) {
-                            std::cout<<"bin op\n";
-                        }
-                        else if(const DeclStmt* declStmt = dyn_cast<DeclStmt>(stmt)){
-                            std::cout<<"decl stmt\n";
-                        }
-
-                    }
-                    
-                }               
-            }
-        }
-    }
+    void statCFGs();
 
 };
 
-#endif
+#endif // end of CFG_STAT_H
--- lib/Stat/CFGStat.cpp
+++ lib/Stat/CFGStat.cpp
@@ -1,1 +1,37 @@
+#include "Stat/CFGStat.h"
 
+
+
+void CFGStat::statCFGs() {
+    for(auto fun : callgraph->getTopLevelFunctions()){
+        auto& cfg=manager->getCFG(fun);
+        if(cfg==NULL){
+            continue;
+        }
+        dumpCFG(cfg);
+        for (auto I = cfg->begin(); I != cfg->end() ; I++) {
+            // Skip the entry block, because we already printed it.
+            if (&(**I) == &cfg->getEntry() || &(**I) == &cfg->getExit())
+                continue;
+            const CFGBlock *block=*I;
+            for(auto eleIt=block->begin(); eleIt!=block->end(); eleIt++){
+                auto& ele=*eleIt;
+                auto kind=ele.getKind();
+                eleIt->dump();
+                if(kind==CFGElement::Kind::Statement){
+                    CFGStmt cfgStmt = ele.castAs<CFGStmt>();
+                    const Stmt *stmt = cfgStmt.getStmt();
+                    assert(stmt != nullptr && "Expecting non-null Stmt");
+                    if (const BinaryOperator* binOp = dyn_cast<BinaryOperator>(stmt)) {
+                        std::cout<<"bin op\n";
+                    }
+                    else if(const DeclStmt* declStmt = dyn_cast<DeclStmt>(stmt)){
+                        std::cout<<"decl stmt\n";
+                    }
+
+                }
+                
+            }               
+        }
+    }
+}
